4b0b523fb846e072e551c63ae527487b5f0a1a3c
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-12-31 21:37:51 +0530

    fix: changes as frontend asked

--- backend/src/rewards/getAllPointsHistory.js
+++ backend/src/rewards/getAllPointsHistory.js
@@ -4,25 +4,37 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
+    let page = req.query.page || 1;
+    page = parseInt(page);
+    page = page < 1 ? 1 : page;
+
+    let limit = req.query.limit || 50;
+    let offset = (page - 1) * limit;
     const response = await prisma.owners.findMany({
       orderBy: {
-        points: "desc", 
+        points: "desc",
       },
       select: {
         id: true,
         points: true,
       },
+      take: limit,
+      skip: offset,
     });
 
+    let totalAssets = await prisma.owners.count();
+
+    let totalPage = Math.ceil(totalAssets / limit);
+
     res.send({
-      message: response,
+      data: response,
+      totalPage: totalPage,
+      nextPage: page + 1 > totalPage ? null : page + 1,
     });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: error.message });
   }
 });
-
-
 
 module.exports = router;
