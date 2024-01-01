15b3b00abfec7a58c17f6999be744ff325c812fd
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-01-02 01:24:02 +0530

    fix: showCollections conflict

--- backend/src/collections/showCollections.js
+++ backend/src/collections/showCollections.js
@@ -4,8 +4,50 @@ const prisma = require("../prisma");
 
 router.get("/", async (req, res) => {
   try {
-    let collectionAddress = req.params.collection;
+    let collectionAddress = parseInt(req.query.collection);
 
+    // let page = req.query.page || 1;
+    // page = parseInt(page);
+    // page = page < 1 ? 1 : page;
+
+    // let limit = req.query.limit || 50;
+    // let offset = (page - 1) * limit;
+
+    let collections = await prisma.collections.findMany();
+
+    // let contents = await prisma.contents.findMany({
+    //   take: limit,
+    //   skip: offset,
+    //   orderBy: {
+    //     createdAt: "desc",
+    //   },
+    //   where: {
+    //     collectionId: collections.id,
+    //   },
+    // });
+
+    // let totalAssets = await prisma.contents.count({
+    //   where: {
+    //     collectionId: collections.id,
+    //   },
+    // });
+
+    // let totalPage = Math.ceil(totalAssets / limit);
+
+    res.send({
+      message: collections,
+    });
+  } catch (error) {
+    console.error("Error handling request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/collection", async (req, res) => {
+  try {
+    let id = parseInt(req.query.id);
+    console.log(req.query);
+    
     let page = req.query.page || 1;
     page = parseInt(page);
     page = page < 1 ? 1 : page;
@@ -13,12 +55,9 @@ router.get("/", async (req, res) => {
     let limit = req.query.limit || 50;
     let offset = (page - 1) * limit;
 
-    let collections = await prisma.collections.findFirst({
+    let collection = await prisma.collections.findUnique({
       where: {
-        address: {
-          equals: collectionAddress,
-          mode: "insensitive",
-        },
+        id: id,
       },
     });
 
@@ -29,17 +68,21 @@ router.get("/", async (req, res) => {
         createdAt: "desc",
       },
       where: {
-        collectionId: collections.id,
+        collectionId: id,
       },
     });
 
     let totalAssets = await prisma.contents.count({
       where: {
-        collectionId: collections.id,
+        collectionId: id,
       },
     });
 
     let totalPage = Math.ceil(totalAssets / limit);
+
+    if (!collection) {
+      return res.status(500).json({ error: "Collection not found" });
+    }
 
     res.send({
       assets: contents,
