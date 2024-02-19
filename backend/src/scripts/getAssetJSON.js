10a7c0cd7cef1dd02eb11298a28b1932643fe6cf
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-02-19 23:05:59 +0530

    fix: getAssetJSON as per frontend

--- backend/src/scripts/getAssetJSON.js
+++ backend/src/scripts/getAssetJSON.js
@@ -9,14 +9,19 @@ const router = express.Router();
 const processAssetJSON = async (requestData) => {
   const { stickerAuthor, type, data, tags, wallet, campaign, featured } =
     requestData;
+
   const tagsArray = Array.isArray(tags) ? tags : [];
 
-  const assetJSONPromises = data.map(async (item) => {
-    const response = await axios.get(item, { responseType: "arraybuffer" });
-    const dimensions = sizeOf(response.data);
+  const promiseArray = data.map((item) => {
+    return axios.get(item.trim(), { responseType: "arraybuffer" });
+  });
 
-    const asset = {
-      image: item,
+  let asset = await Promise.all(promiseArray);
+
+  for (let i = 0; i < data.length; i++) {
+    const dimensions = sizeOf(asset[i].data);
+    const assetData = {
+      image: data[i].trim(),
       tags: tagsArray,
       author: stickerAuthor,
       type,
@@ -26,23 +31,14 @@ const processAssetJSON = async (requestData) => {
       campaign,
     };
 
-    return JSON.stringify(asset);
-  });
-
-  let asset = await Promise.all(assetJSONPromises);
-  asset = JSON.parse(asset);
-  try {
-    await prisma.assets.createMany({
-      data: asset,
-      skipDuplicates: true,
-    });
-
-    console.log("Data uploaded to the database.");
-    return { message: "Data uploaded to the database." };
-  } catch (error) {
-    console.error("Error uploading data:", error);
-    throw error;
+    // await prisma.assets.createMany({
+    //   data: assetData,
+    //   skipDuplicates: true,
+    // });
   }
+
+  // console.log("Data uploaded to the database.");
+  return { message: "Data uploaded to the database." };
 };
 
 router.post("/", auth, async (req, res) => {
