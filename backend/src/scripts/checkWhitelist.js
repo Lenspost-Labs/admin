003bbf193013abe84f4bea09ee6859580495a8c7
MrPreet <josonpreet@gmail.com>
2024-10-19 19:12:40 +0530

    add new email in whitelist

--- backend/src/scripts/checkWhitelist.js
+++ backend/src/scripts/checkWhitelist.js
@@ -1,28 +1,34 @@
-const express = require("express");
-const router = express.Router();
-const generateJwt = require("../utils/generateJwt");
+const express = require('express')
+const router = express.Router()
+const generateJwt = require('../utils/generateJwt')
 
-const whitelistedEmails = ["sugatobagchi@dsec.ac.in", "tirumaladasa@gmail.com", "aryan@lenspost.xyz", "chakra@lenspost.xyz", "cybershakti@lenspost.xyz"];
+const whitelistedEmails = [
+	'sugatobagchi@dsec.ac.in',
+	'tirumaladasa@gmail.com',
+	'aryan@lenspost.xyz',
+	'chakra@lenspost.xyz',
+	'cybershakti@lenspost.xyz',
+	'josonpreet@gmail.com',
+]
 
-router.post("/", async (req, res) => {
-  try {
-    const { email } = req.body; 
+router.post('/', async (req, res) => {
+	try {
+		const { email } = req.body
 
-    if (!email) {
-      return res.status(400).json({ error: "Email not provided" });
-    }
+		if (!email) {
+			return res.status(400).json({ error: 'Email not provided' })
+		}
 
-    if (whitelistedEmails.includes(email.toLowerCase())) {
+		if (whitelistedEmails.includes(email.toLowerCase())) {
+			const token = generateJwt(email)
+			res.status(200).json({ whitelisted: true, token })
+		} else {
+			res.status(200).json({ whitelisted: false })
+		}
+	} catch (error) {
+		console.error('Error in authentication', error)
+		res.status(500).json({ error: error.message })
+	}
+})
 
-      const token = generateJwt(email);
-      res.status(200).json({ whitelisted: true, token });
-    } else {
-      res.status(200).json({ whitelisted: false });
-    }
-  } catch (error) {
-    console.error("Error in authentication", error);
-    res.status(500).json({ error: error.message });
-  }
-});
-
-module.exports = router;
+module.exports = router
